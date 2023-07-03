58e1615d65a62caab57bafd9ae29d307e2ca569d
rootc137 <pessicksanchez@gmail.com>
2023-07-03 11:38:35 +0200

    Homepage prototype..

---
+++ FRONT/argentbank/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import {Hero} from "../../components/Hero/Hero";
+
+
+import './Main.css';
+import {Features} from "../../components/Features/Features";
+
+export const Home = () =>
+{
+    return(
+        <main>
+            <Hero />
+            <Features />
+        </main>
+    );
+}
